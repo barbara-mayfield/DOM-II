3e2d6be49248d26b50948711c673a46c2362ae34
Barbara Mayfield <bmayfield1991@gmail.com>
2019-08-08 01:58:01 -0500

    added event listeners to .nav-link to change text colors in index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+
+const navTags = Array.from(document.getElementsByClassName('nav-link'))
+    .forEach(function(navTags){
+        navTags.addEventListener("mouseenter", e => {e.target.style.color = 'goldenrod'});
+        navTags.addEventListener("mouseleave", e => {e.target.style.color = 'black'});
+    });
