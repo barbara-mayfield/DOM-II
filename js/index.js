db37b9aa8a6908f242058c28ff2813e3d1080648
Barbara Mayfield <bmayfield1991@gmail.com>
2019-08-08 23:23:45 -0500

    added 'mousewheel' function to images, 'dragend' & 'dblclick' to funBusImg, & a 'mousedown' event to the header logo

--- js/index.js
+++ js/index.js
@@ -5,36 +5,88 @@ const bodyStyle = document.getElementsByTagName('body');
 for (var i = 0 ; i < bodyStyle.length; i++) {
     bodyStyle[i].addEventListener('wheel', e => {e.target.style.backgroundColor = '#DEECF2';});
  }
-console.log(bodyStyle);
+// console.log(bodyStyle);
 
 // 2 & 3 'mouseenter', 'mouseleave'
 const navTags = Array.from(document.getElementsByClassName('nav-link'));
 navTags.forEach(function(navTags){
-        navTags.addEventListener('mouseenter', e => {e.target.style.color = 'goldenrod'});
-        navTags.addEventListener('mouseleave', e => {e.target.style.color = 'black'});
+        navTags.addEventListener('mouseenter', e => {
+            e.target.style.color = 'goldenrod'
+        });
+        navTags.addEventListener('mouseleave', e => {
+            e.target.style.color = 'black'
+        });
     });
 
-// 4 'mouseover'
+// 4 & 5 'mouseover', 'mousewheel'
 const images = Array.from(document.getElementsByTagName('img'));
 images.forEach(function(images) {
-    images.addEventListener('mouseover', e => {e.target.style.borderRadius = '5rem';})
+    images.addEventListener('mouseover', e => {e.target.style.borderRadius = '5rem';});
+    images.addEventListener('mousewheel', mouseWheelHandler, false);
+    
+    function mouseWheelHandler(e) {
+        var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
+        images.style.width = Math.max(50, Math.min(800, images.width + (30 * delta))) + "px";
+        return false;
+    };
 });
-console.log(images);
+    
+// console.log(images);
 
-// 5 'drag'
+// 6, 7 & 8 'drag', 'dragend' 'dblclick'
 const funBusImg = document.querySelector('.intro img');
 funBusImg.addEventListener('drag', e => {
     e.target.style.display = 'none';});
-console.log(funBusImg);
+funBusImg.addEventListener('dragend', e => {
+    e.target.style.display = 'flex';});
+funBusImg.addEventListener('dblclick', e => {
+    e.target.style.opacity = '0.8'});
+// console.log(funBusImg);
 
-// 6 'click'
+const dontDoItP = document.createElement('p');
+dontDoItP.textContent = `Don't drag the car!`;
+dontDoItP.style.font = '"Times New Roman", Times, Serif';
+dontDoItP.style.fontStyle = 'italic';
+dontDoItP.style.marginLeft = '42%';
+document.querySelector('.intro h2').prepend(dontDoItP);
+
+// 9 'click'
 const buttonStyle = document.querySelectorAll('.btn');
 buttonStyle.forEach(function(buttonStyle){
     buttonStyle.addEventListener('click', e => {
+        e.stopPropagation();
         e.target.style.border = '3px solid #FFEBCD', 
         e.target.textContent = `You've Clicked Here!`});
     buttonStyle.addEventListener('mouseleave', e => {
         e.target.style.border = '1px solid #FFEBCD',
         e.target.textContent = `Sign Me Up!`});
 });
-console.log(buttonStyle);
+// console.log(buttonStyle);
+
+// 10 'mousedown'
+
+const logoStyle = document.querySelector('.logo-heading');
+logoStyle.addEventListener('mousedown', e => {
+    e.target.textContent = 'Super Fun Bus!'});
+
+// Extra Work
+
+var navButton = document.createElement('button'), count = 0;
+document.querySelector('.home .intro').prepend(navButton);
+navButton.style.width = '15rem';
+navButton.style.height = '2rem';
+navButton.style.marginTop = '2rem';
+navButton.style.marginRight = '38rem';
+navButton.style.marginBottom = '2rem';
+navButton.style.marginLeft = '32rem';
+navButton.style.textAlign = 'center';
+navButton.addEventListener('click', (e) => {
+    count += 1;
+    navButton.textContent = "Click Count:" + count;
+});
+
+navButton.addEventListener('mousemove');
+
+
+const navStyle = document.querySelector('.main-navigation');
+console.log(navStyle);
